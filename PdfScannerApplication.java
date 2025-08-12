3be1d1055978e24ce81bfbd265e08a559f296b79
Yatharth-Amar <92916287+Yatharth-Amar@users.noreply.github.com>
2025-08-12 20:28:39 +0530

    Add files via upload

---
+++ PdfScannerApplication.java
@@ -0,0 +1,11 @@
+package com.example.pdfscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfScannerApplication {
+public static void main(String[] args) {
+        SpringApplication.run(PdfScannerApplication.class, args);
+    }
+}
